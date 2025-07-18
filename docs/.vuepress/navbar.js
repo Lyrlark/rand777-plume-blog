131e980c2a96613d3d00f86b11e39673228a2969
rand777 <losmosga@foxmail.com>
2025-07-19 01:19:16 +0800

    docs: improve docs

--- docs/.vuepress/navbar.js
+++ docs/.vuepress/navbar.js
@@ -2,11 +2,17 @@ import {defineNavbarConfig} from 'vuepress-theme-plume'
 
 export const navbar = defineNavbarConfig([
     {text: '首页', link: '/', icon: 'material-symbols:home'},
-    {text: '博客', link: '/blog/', icon: 'material-symbols:article-outline', activeMatch: '^(/blog/|/article/)',},
+    {
+        text: '博客',
+        link: '/blog/',
+        icon: 'material-symbols:article-outline',
+        activeMatch: '^(/blog/|/article/)',
+    },
+    {text: 'Projects', link: '/more/projects/', icon: 'el:opensource'},
     {
         text: '笔记',
         items: [
-    {text: '每日英语', link: '/memo/everyday-english/',icon: 'icon-park-outline:english'},
+            {text: '每日英语', link: '/memo/everyday-english/', icon: 'icon-park-outline:english'},
 
         ],
         icon: 'ic:outline-note-alt',
@@ -16,8 +22,8 @@ export const navbar = defineNavbarConfig([
         text: '更多',
         items: [
             // {text: '关于博客', link: '/more/about-blog/', icon: 'material-symbols:person-outline'}, TODO: 需要更新下博客页结构
-            {text: '友情链接', link: '/more/friends/', icon: 'icon-park:friends-circle'},
-            {text: '开源项目', link: '/more/projects/', icon: 'material-icon-theme:folder-project-open'},
+            {text: '友情链接', link: '/more/friends/', icon: 'fluent:people-16-filled'},
+
             {text: '网址导航', link: '/more/sites-collect/', icon: 'logos:sitepoint'},
         ],
         icon: 'mingcute:more-3-fill',
--- docs/notes/more/projects.md
+++ docs/notes/more/projects.md
@@ -6,27 +6,26 @@ permalink: /more/projects/
 
 ## 开源项目
 
+[//]: # (TODO: 添加更多开源项目)
+
 > also see : [https://rand777.pguide.studio](https://rand777.pguide.studio)
 
-### PGuide-Docs
+### @LyrLark/pguide-docs
 
 `PGuide-Docs` 是一个开源的文档类项目，旨在为 PGuide 和 CQMUer提供全面的文档支持。
 
 <RepoCard repo="Lyrlark/PGuide-Docs"></RepoCard>
 
-### when2eat
+### @PGuideDev/when2eat
 
 `when2eat` 是一个开源的餐饮预约推荐与可视化系统，旨在帮助用户更方便地选择吃饭的时间区间。
 
 <RepoCard repo="PGuideDev/when2eat"></RepoCard>
 
-### 
+### @CQMUA/server-center
 
-<RepoCard repo=""></RepoCard>
-<RepoCard repo=""></RepoCard>
+<RepoCard repo="CQMUA/server-center"></RepoCard>
 
+### @DingGe3/oh-my-api   
 
-## 渝高联开源项目
-
-## LaTeX 模板
-
+<RepoCard repo="DingGe3/Oh-My-Api"></RepoCard>
